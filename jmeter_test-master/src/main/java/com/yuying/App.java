a170e1fa253c568d7c289c3257adae04ef32dd80
tanjiawen <tanjiawen126@126.com>
2018-10-31 10:23:37 +0800

    Signed-off-by: tanjiawen <tanjiawen126@126.com>

---
+++ jmeter_test-master/src/main/java/com/yuying/App.java
@@ -0,0 +1,13 @@
+package com.yuying;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
