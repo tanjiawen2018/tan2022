6ae69c4d0639eeacba0d8db500e16a33c37157e0
tanjiawen126 <tanjiawen123@126.com>
2022-07-09 04:25:39 +0800

    add Spring...

---
+++ SpringbootDemo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("test")
+    public String test(){
+        return "第一个springboot程序搭建成功";
+    }
+}
